3da02ab1fe11d0912eddaff4aa890ce489737d99
yiyun <yiyungent@gmail.com>
2021-06-22 16:35:55 +0800

    feat(js/util.js): update

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -11,7 +11,7 @@ util.httpGet = function(url,success,error){
     });
 };
 
-// post 方式发送, 内容以 json 格式组织: {a:1,b:2} 放于 请求体
+// post 方式发送, 内容以 json 格式组织: {'name':'jack','sex':'man'} 放于 请求体
 util.httpPost = function(url,data,success,error){
     $.ajax({
         url:url,
@@ -25,21 +25,33 @@ util.httpPost = function(url,data,success,error){
     });
 };
 
-// post 方式发送, 内容以 form-data 格式组织: a=1&b=c 放于 请求体
+
+// post 方式发送, 内容以 form-data 格式组织: key1=value1&key2=value2 放于 请求体
+// form-data 对应的是 以form表单提交传值的情形
+// x-www-form-urlencoded
+// 即 application/x-www-from-urlencoded，将表单内的数据转换为 Key-Value。
+// POST  HTTP/1.1
+// Host: test.app.com
+// Content-Type: application/x-www-form-urlencoded
+
+// key1=value1&key2=value2
 util.httpPostForm = function(url,data,success,error){
     $.ajax({
         url:url,
         type: "post",
         async: true,
-        contentType: "application/json", // 发送的内容 的数据格式类型
-        dataType: "json", // 预期服务器返回的数据格式类型, 如果不指定, jQuery 将自动根据 HTTP 包 MIME 信息来智能判断
+        contentType: "application/x-www-form-urlencoded", // 发送的内容 的数据格式类型
         data:data,
         success: success,
         error: error
     });
 };
 
-// post 方式发送, 内容以 form-data 格式组织: a=1&b=c 放于 请求体, contentType 为自动计算，用于多部分文件上传(file=blob, 其后有文件内容定界符)
+
+
+
+// post 方式发送, 内容以 键值对 key1:val1 格式组织: a:1 放于 请求体, contentType 为自动计算，用于多部分文件上传(file=blob, 其后有文件内容定界符boundary)
+// multipart/form-data: 由于有boundary隔离，所以multipart/form-data既可以上传文件，也可以上传键值对，它采用了键值对的方式，所以可以上传多个文件
 util.httpFormData = function(url,data,success,error){
     $.ajax({
         url:url,
