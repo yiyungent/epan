afc46a6b3c2b107e622af4aa2d5afdf593c1c489
yiyun <yiyungent@gmail.com>
2021-06-22 15:02:59 +0800

    style(js/main.js): add annotation

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -277,6 +277,7 @@ function openFile(path, fileName) {
 
 function dropEvent(e) {
     e.preventDefault(); //取消默认浏览器拖拽效果
+    // 文件列表: 拖进去的可能是多个文件
     let fileList = e.dataTransfer.files; //获取文件对象
     //检测是否是拖拽文件到页面的操作
     if (fileList.length === 0) {
@@ -285,6 +286,7 @@ function dropEvent(e) {
     console.log(fileList.length);
 
     for (let i = 0;i < fileList.length;i++) {
+        // 遍历 每个文件
         fileRead(pathDom.value, fileList[i]);
     }
 }
@@ -331,18 +333,24 @@ function updateFile(path,file) {
             <div id="{4}" class="progress-bar progress-bar-striped" style="width: 0%;"  aria-valuemin="0" aria-valuemax="100"></div>
         </div>`;
 
+    // 文件总大小
     let totalSize = file.size,
         filename = file.name,
+        // 文件绝对路径 (云盘中的路径)
         fileAbs = path+"/"+filename,
+        // 切片个数
         total = Math.ceil(file.size / sliceSize),
         existBlob = new Map(),
         loadedMap = new Map();
 
 
-    console.log(filename,totalSize,total,fileAbs);
+    // console.log(filename,totalSize,total,fileAbs);
+    console.log(util.format("文件名: {0} ,文件大小: {1} ,切片个数: {2} ,文件绝对路径: {3}",filename,totalSize,total,fileAbs));
+
     function addProgress() {
         document.getElementById('update-info').style.display = "block";
         if (!updateInfos.has(fileAbs)) {
+            // 若更新进度信息 中 无 此绝对路径的文件，则添加到 上传进度中
             let list = document.getElementById('update-list');
             let pSize = util.b2string(totalSize,1024);
             let index = path.lastIndexOf("\/");
@@ -376,11 +384,14 @@ function updateFile(path,file) {
         if (loaded === totalSize){
             document.getElementById(pBar).style.width = "0%";
             if (isFinished) {
+                // 上传完成刷新文件列表
                 refresh();
             }
             if (isNeed){
+                // 需要继续上传 -> 普通上传
                 document.getElementById(pWidth).innerHTML = `<i class="fa fa-check-circle" style="color: #00CC00"></i>`
             }else {
+                // 不需要继续上传 -> 秒传
                 document.getElementById(pWidth).innerHTML = `<i class="fa fa-check-circle" style="color: #00CC00">秒传</i>`
             }
         }else {
@@ -400,14 +411,17 @@ function updateFile(path,file) {
     }
 
     function checkFile(md5,callback) {
-        // TODO: 文件上传检查
+        // 文件上传检查
         let reqUrl = httpAddr +"/api/file/uploadCheck";
         let cmd = {path:path,fileName:filename,total:total,fileSignKey:md5,size:totalSize};
         util.httpPost(reqUrl,JSON.stringify(cmd), function (res) {
             if (res.code > 0){
                 if (res.code == 2){
+                    // 需要继续上传
+                    // TODO: res.upload = exist
                     callback(true,res.upload)
                 }else {
+                    // 秒传
                     callback(false)
                 }
             }else {
@@ -420,45 +434,73 @@ function updateFile(path,file) {
         checkFile(md5,function (need,exist) {
             addProgress();
             if (need){
+                // 需要 上传
+                // 来自服务端响应的 已上传的 切片序号 json; 存在已有切片 (以前上传过此文件，已有此文件的部分切片)
+                // 猜测 exist 格式如下:
+                // {
+                //  2: 313312, // 313312 为此切片大小
+                //  5: 131233
+                // }
+                // 此处，即，需要上传，但已有部分切片, 只需上传缺失的切片
                 if (exist) {
-                    console.log(exist);
+                    console.log("来自服务端响应的 此文件 已上传的 切片序号数组: ", exist);
                     for (let key in exist){
+                        // 注意: key 是 切片序号
                         existBlob.set(key, key);
+                        // 已上传 切片: (切片序号, 切片大小)
                         loadedMap.set(key,sliceSize);
                     }
                 }
 
                 let reqUrl = httpAddr +"/api/file/upload";
+                // 切片序号: 这里赋值0，但 实际序号从1开始, 因为后面循环是先加1
                 let current = 0;
+                // 循环 上传 每个切片
                 while (current < total) {
                     current++;
                     if (!existBlob.has(current.toString())){
+                        // 已存在的切片中 没有当前切片 才上传
                         let start = sliceSize * (current-1);
                         let end = sliceSize * current;
+                        // 切片终止位置 不可能大于文件总大小 -> 截断
+                        // 最后一个切片实际大小只能是<=切片固定大小
+                        // 最后一个切片终止位置 即 文件末位置
                         end = end > totalSize ? totalSize : end;
+                        // 从文件中按 起始终止位置 切片
                         let blob = file.slice(start, end);
 
                         let xhr =  new XMLHttpRequest();
                         let fd = new FormData();
+                        // TODO: 切片 绝对路径
                         fd.append('path',path);
+                        // 切片
                         fd.append('file', blob);
+                        // 切片所在文件的 文件名
                         fd.append('fileName', filename);
+                        // 切片序号
                         fd.append('current', current.toString());
+                        // 切片所在文件的 文件md5
                         fd.append('fileSignKey', md5);
-
+                        // 上传 切片
                         xhr.open("post",reqUrl,true);
                         xhr.onreadystatechange = function () {
                             if (xhr.readyState === 4) {
                                 if (xhr.status === 200) {
+                                    // 保存当前上传成功的 切片: (此切片序号, 此切片实际大小)
                                     loadedMap.set(fd.get("current"),blob.size);
+                                    // TODO: 更新进度
+                                    // Q: 奇怪，这里只是上传了一个切片，为何就传递参数 isFinished: true
+                                    // A: 其实可以看下面，在上传进度中时，不断更新为"未完成"，因此只有当所有切片上传完成后，实际才会被更新为 "完成"
                                     setProgress(true,true);
                                 } else {
                                     showTips("网络错误！",1000)
                                 }
                             }
                         };
+                        // TODO: 上传进度中
                         xhr.upload.onprogress = function(e) {
                             loadedMap.set(fd.get("current"),e.loaded);
+                            // 更新进度: "未完成"
                             setProgress(true,false);
                         };
                         xhr.send(fd)
@@ -466,6 +508,7 @@ function updateFile(path,file) {
                 }
 
             }else {
+                // 不需要 上传, "完成"
                 setProgress(false,true);
             }
         })
